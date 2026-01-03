864d44325495fdcaeb15120681c5087e8d86e0b9
shrestha <shresthachakrabortykgp@gmail.com>
2026-01-03 22:12:44 +0530

    AOD model

---
+++ Backend/preprocessing/enhance.py
@@ -0,0 +1,8 @@
+import cv2
+
+def apply_clahe(img):
+    lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(lab)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    l = clahe.apply(l)
+    return cv2.cvtColor(cv2.merge((l,a,b)), cv2.COLOR_LAB2BGR)
